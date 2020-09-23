5287fa2c237f9bdd5a6e2a8644dfd92723a24b2f
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-09-23 12:56:40 +0530

    initial commit

---
+++ scriptdashboard.js
@@ -0,0 +1,16 @@
+function shorten(e){
+
+	e.preventDefault();
+	console.log("Shortening")
+	var url = document.querySelector("#url").value;
+	var key ;
+	//key = document.querySelector("#key").value;
+
+	if(key == "" || key == undefined){
+		key = Math.random().toString(36).substr(2, 5);
+	}
+
+	fetch(`/shorten?url=${url}&key=${key}`);
+	
+	document.querySelector("h4").innerHTML = `Shortened URL is <a href="https://small.tiny/${key}">small.tiny/${key}</a>`;
+}
